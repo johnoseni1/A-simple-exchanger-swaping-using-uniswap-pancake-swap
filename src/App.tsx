0b92885b3fa1e0e57fc152d18b24658da47f8050
John Oseni <johnoseni26@gmail.com>
2022-01-05 23:03:51 +0100

    changed

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ function App() {
   return (
     <div className="App">
       <div className="Banner">
-        <div className="Banner-description">
-          Swap is unaudited software. Use at your own risk.
-        </div>
+   
       </div>
       <Routes />
       <div className="social-buttons">
